d093b85c1dc68cced7a0cb02796bcbf62e98aae0
Yogeshwaran <yogeshwaran08083@gmail.com>
2024-05-02 14:57:29 +0530

    design complete

--- src/Pages/Body/Index.tsx
+++ src/Pages/Body/Index.tsx
@@ -1,5 +1,12 @@
 import Lottie from "lottie-react";
 import SchoolAnimation from "../../assets/animation/school-animation.json";
+import Graduation from "../../assets/animation/graduation.json";
+import Illustrator from "../../assets/animation/illustrator.json";
+import Figma from "../../assets/animation/figma.json";
+import UIUX from "../../assets/animation/UIUX_design.json";
+import WebDesigning from "../../assets/animation/Web designing.json";
+import MobileDesign from "../../assets/animation/Mobile_design.json";
+import Innovative from "../../assets/animation/Innovative.json";
 import "./body.css";
 import jupyter from "../../assets/imgs/jupyter.png";
 import astronut from "../../assets/imgs/astronut.png";
@@ -16,87 +23,169 @@ const Body = () => {
       <div className="card card-1">
         <div className="glass"></div>
         <div className="frost"></div>
-        <div className="content-vertical">
+        <div className="vertical-content">
           <Lottie
             animationData={SchoolAnimation}
             loop={true}
             className="grid-1-animation"
           />
-          <span>
-            <h3 style={{ color: "white" }}>School education</h3>
+          <div>
+            <h3>School education</h3>
             <p>
               Lorem Ipsum is simply dummy text of the printing and typesetting
               industry. Lorem Ipsum has been the industry's standard dummy text
               ever since the 1500s,
             </p>
-          </span>
+          </div>
         </div>
       </div>
 
       <div className="card card-2">
         <div className="glass"></div>
         <div className="frost"></div>
+        <div className="vertical-content">
+          <Lottie
+            animationData={Graduation}
+            loop={true}
+            className="grid-1-animation"
+          />
+          <div>
+            <h3>College Education</h3>
+            <p>
+              Lorem Ipsum is simply dummy text of the printing and typesetting
+              industry. Lorem Ipsum has been the industry's standard dummy text
+              ever since the 1500s,
+            </p>
+          </div>
+        </div>
       </div>
 
       <div className="card card-3">
         <div className="glass"></div>
         <div className="frost"></div>
-        <div className="content"></div>
+        <div className="horizontal-content">
+          <Lottie
+            animationData={Illustrator}
+            loop={true}
+            className="grid-3-animation"
+          />
+          <div>
+            <h3>Adobe Illustrator</h3>
+            <p>
+              Lorem Ipsum is simply dummy text of the printing and typesetting
+              industry. Lorem Ipsum has been the industry's standard dummy text
+              ever since the 1500s,
+            </p>
+          </div>
+        </div>
       </div>
 
       <div className="card card-4">
         <div className="glass"></div>
         <div className="frost"></div>
-        <div className="content">{/* Content of the card */}</div>
+        <div className="horizontal-content">
+          <Lottie
+            animationData={Figma}
+            loop={true}
+            className="grid-3-animation"
+          />
+          <div>
+            <h3>Figma</h3>
+            <p>
+              Lorem Ipsum is simply dummy text of the printing and typesetting
+              industry. Lorem Ipsum has been the industry's standard dummy text
+              ever since the 1500s,
+            </p>
+          </div>
+        </div>
       </div>
 
       <div className="card card-5">
         <div className="glass"></div>
         <div className="frost"></div>
-        <div className="content">{/* Content of the card */}</div>
+        <div className="horizontal-content">
+          <Lottie
+            animationData={UIUX}
+            loop={true}
+            className="grid-3-animation"
+          />
+          <div>
+            <h3>Design Principals</h3>
+            <p>
+              Lorem Ipsum is simply dummy text of the printing and typesetting
+              industry. Lorem Ipsum has been the industry's standard dummy text
+              ever since the 1500s,
+            </p>
+          </div>
+        </div>
       </div>
 
       <div className="card card-6">
         <div className="glass"></div>
         <div className="frost"></div>
-        <div className="content">{/* Content of the card */}</div>
+        <div className="vertical-content">
+          <Lottie
+            animationData={WebDesigning}
+            loop={true}
+            className="grid-1-animation"
+          />
+          <div>
+            <h3>Web designing</h3>
+            <p>
+              Lorem Ipsum is simply dummy text of the printing and typesetting
+              industry. Lorem Ipsum has been the industry's standard dummy text
+              ever since the 1500s,
+            </p>
+          </div>
+        </div>
       </div>
 
       <div className="card card-7">
         <div className="glass"></div>
         <div className="frost"></div>
-        <div className="content">{/* Content of the card */}</div>
+        <div className="vertical-content">
+          <Lottie
+            animationData={MobileDesign}
+            loop={true}
+            className="grid-1-animation"
+          />
+          <div>
+            <h3>Mobile Design</h3>
+            <p>
+              Lorem Ipsum is simply dummy text of the printing and typesetting
+              industry. Lorem Ipsum has been the industry's standard dummy text
+              ever since the 1500s,
+            </p>
+          </div>
+        </div>
       </div>
 
       <div className="card card-8">
         <div className="glass"></div>
         <div className="frost"></div>
-        <div className="content">{/* Content of the card */}</div>
+        <div className="horizontal-content">
+          <Lottie
+            animationData={Innovative}
+            loop={true}
+            className="grid-3-animation"
+          />
+          <div>
+            <h3>Innovative thinker</h3>
+            <p>
+              Lorem Ipsum is simply dummy text of the printing and typesetting
+              industry. Lorem Ipsum has been the industry's standard dummy text
+              ever since the 1500s,
+            </p>
+          </div>
+        </div>
       </div>
-
-      <div className="card card-9">
-        <div className="glass"></div>
-        <div className="frost"></div>
-        <div className="content">{/* Content of the card */}</div>
-      </div>
-
-      <div className="card card-10">
-        <div className="glass"></div>
-        <div className="frost"></div>
-        <div className="content">{/* Content of the card */}</div>
-      </div>
-
-      <div className="card card-11">
-        <div className="glass"></div>
-        <div className="frost"></div>
-        <div className="content">{/* Content of the card */}</div>
-      </div>
-
       <div className="floating-img right-slanding-img astronut-position">
         <motion.img
           src={astronut}
           alt="Astroid"
           className="astronut-img"
+          initial={{ translateX: -100 }}
+          whileInView={{ translateX: 0, transition: { duration: 2 } }}
           animate={{
             y: [-5, 5, -5],
             transition: { yoyo: true, duration: 3, repeat: Infinity },
@@ -109,6 +198,12 @@ const Body = () => {
           src={rocket}
           alt="Rcoket"
           className="rocket-img"
+          initial={{ translateX: 10, translateY: 200 }}
+          whileInView={{
+            translateX: 0,
+            translateY: 0,
+            transition: { duration: 1.5 },
+          }}
           animate={{
             y: [-5, 5, -5],
             transition: { yoyo: true, duration: 3, repeat: Infinity },
@@ -134,6 +229,8 @@ const Body = () => {
           src={jupyter}
           alt="Astroid"
           className="jupyter-img"
+          initial={{ translateX: 100 }}
+          whileInView={{ translateX: 0, transition: { duration: 2 } }}
           animate={{
             y: [-5, 5, -5],
             transition: { yoyo: true, duration: 3, repeat: Infinity },
@@ -146,6 +243,8 @@ const Body = () => {
           src={saturn}
           alt="Saturn"
           className="saturn-img"
+          initial={{ translateX: -100 }}
+          whileInView={{ translateX: 0, transition: { duration: 2 } }}
           animate={{
             y: [-5, 5, -5],
             transition: { yoyo: true, duration: 3, repeat: Infinity },
@@ -158,6 +257,8 @@ const Body = () => {
           src={mars}
           alt="Mars"
           className="mars-img"
+          initial={{ translateX: 50 }}
+          whileInView={{ translateX: 0, transition: { duration: 2 } }}
           animate={{
             y: [-5, 5, -5],
             transition: { yoyo: true, duration: 3, repeat: Infinity },
@@ -170,6 +271,8 @@ const Body = () => {
           src={astroid1}
           alt="Astroid"
           className="astroid1-img"
+          initial={{ translateX: -70 }}
+          whileInView={{ translateX: 0, transition: { duration: 2 } }}
           animate={{
             y: [-5, 5, -5],
             transition: { yoyo: true, duration: 3, repeat: Infinity },
@@ -182,6 +285,8 @@ const Body = () => {
           src={astroid2}
           alt="Astroid"
           className="astroid2-img"
+          initial={{ translateY: 70 }}
+          whileInView={{ translateY: 0, transition: { duration: 2 } }}
           animate={{
             y: [-5, 5, -5],
             transition: { yoyo: true, duration: 3, repeat: Infinity },
