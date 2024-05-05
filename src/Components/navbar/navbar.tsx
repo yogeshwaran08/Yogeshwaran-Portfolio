7d2c8ee3334478ab9403a298889f12af5e927ac2
Yogeshwaran <yogeshwaran08083@gmail.com>
2024-05-05 15:49:35 +0530

    added resume

--- src/Components/navbar/navbar.tsx
+++ src/Components/navbar/navbar.tsx
@@ -7,15 +7,26 @@ function NavBar() {
   return (
     <div className="navbar-container">
       <h1 className="navbar-branding">Lydia</h1>
-      <motion.a
-        id="invite-button"
-        whileHover={{ scale: 1.05 }}
-        whileTap={{ scale: 0.9 }}
-        href="https://survey.qwary.com/form/S_wSzSPnasH9Wc_FT15X0J1BuEcPl5gIDLyvUyVtmEo="
-        target="_blank"
-      >
-        <Marquee speed={40}>Let's work Together</Marquee>
-      </motion.a>
+      <div className="nav-btns">
+        <motion.a
+          id="download-resume-btn"
+          whileHover={{ scale: 1.05 }}
+          whileTap={{ scale: 0.9 }}
+          href="https://drive.google.com/file/d/1LOO1X-9ZPdH82E4OucgsENRTvijg3DjZ/view?usp=sharing"
+          target="_blank"
+        >
+          Download resume
+        </motion.a>
+        <motion.a
+          id="invite-button"
+          whileHover={{ scale: 1.05 }}
+          whileTap={{ scale: 0.9 }}
+          href="https://survey.qwary.com/form/S_wSzSPnasH9Wc_FT15X0J1BuEcPl5gIDLyvUyVtmEo="
+          target="_blank"
+        >
+          <Marquee speed={40}>Let's work Together</Marquee>
+        </motion.a>
+      </div>
     </div>
   );
 }
